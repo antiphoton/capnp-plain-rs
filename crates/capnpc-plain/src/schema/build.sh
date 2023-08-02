1f564fb0b7656c8d4da72bc26242f805ad8860dd
Boxiao Cao <9083193+antiphoton@users.noreply.github.com>
2023-08-02 03:20:02 -0700

    Include capnp src

--- crates/capnpc-plain/src/schema/build.sh
+++ crates/capnpc-plain/src/schema/build.sh
@@ -6,5 +6,6 @@ cargo build
 
 CAPNPC_RUST=$PWD/../../../target/debug/capnpc-plain
 CAPNP_TOOL=$PWD/../../../../third_party/capnproto/c++/bazel-bin/src/capnp/capnp_tool
+CAPNP_SRC=$PWD/../../../../third_party/capnproto/c++/src
 
-RUST_BACKTRACE=1 $CAPNP_TOOL compile -o$CAPNPC_RUST schema.capnp
+RUST_BACKTRACE=1 $CAPNP_TOOL compile -o$CAPNPC_RUST -I$CAPNP_SRC schema.capnp
