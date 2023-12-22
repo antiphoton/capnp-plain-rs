947a0c9e9f581bbbb65a7e038476cea22aae0903
Boxiao Cao <9083193+antiphoton@users.noreply.github.com>
2023-12-22 15:34:57 -0800

    Add wire serialization to golden test (#67)

--- packages/golden/src/lib.rs
+++ packages/golden/src/lib.rs
@@ -19,13 +19,63 @@ pub fn get_pretty_json(capnp_bytes: &[u8]) -> String {
 mod tests {
     use std::fs::read;
 
+    use capnp_plain::{
+        message::{
+            tree::struct_node::{get_struct_stack_size, StructNode},
+            word::Word,
+        },
+        CapnpPlainStruct,
+    };
+
     use super::*;
+
+    fn serialize_data(data: &impl CapnpPlainStruct) -> Vec<Word> {
+        let node = data.to_node();
+        let stack_size = get_struct_stack_size(std::slice::from_ref(&node));
+        let serializer = node.to_builder(&stack_size, 0, 0);
+        Message::from(&serializer)
+            .as_flat()
+            .unwrap()
+            .into_iter()
+            .cloned()
+            .collect()
+    }
 
     #[test]
     fn capnp_golden() {
         assert_eq!(
             include_str!("testdata/pretty.json"),
             get_pretty_json(&read("src/testdata/binary").unwrap())
+        );
+    }
+
+    #[test]
+    fn empty_struct() {
+        let data = TestAllTypes::from_node(&StructNode::default());
+        assert_eq!(
+            serialize_data(&data),
+            vec![
+                Word([0, 0, 0, 0, 0, 0, 0x12, 0]),
+                Word([0x45, 0, 0, 0, 0x0a, 0, 0, 0]),
+                Word([0, 0, 0, 0, 0, 0, 0, 0]),
+                Word([0, 0, 0, 0, 0, 0, 0, 0]),
+                Word([0, 0, 0, 0, 0, 0, 0, 0]),
+                Word([0, 0, 0, 0, 0, 0, 0, 0]),
+                Word([0, 0, 0, 0, 0, 0, 0, 0]),
+                Word([0, 0, 0, 0, 0, 0, 0, 0]),
+                Word([0, 0, 0, 0, 0, 0, 0, 0]),
+                Word([0, 0, 0, 0, 0, 0, 0, 0]),
+                Word([0, 0, 0, 0, 0, 0, 0, 0]),
+                Word([0, 0, 0, 0, 0, 0, 0, 0]),
+                Word([0, 0, 0, 0, 0, 0, 0, 0]),
+                Word([0, 0, 0, 0, 0, 0, 0, 0]),
+                Word([0, 0, 0, 0, 0, 0, 0, 0]),
+                Word([0, 0, 0, 0, 0, 0, 0, 0]),
+                Word([0, 0, 0, 0, 0, 0, 0, 0]),
+                Word([0, 0, 0, 0, 0, 0, 0, 0]),
+                Word([0, 0, 0, 0, 0, 0, 0, 0]),
+                Word([0, 0, 0, 0, 0, 0, 0, 0]),
+            ]
         );
     }
 }
