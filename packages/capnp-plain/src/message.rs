1f1a1292310f79be0d4963b1456114f453591be4
Boxiao Cao <9083193+antiphoton@users.noreply.github.com>
2023-12-22 19:06:37 -0800

    Add packing in serialization (#77)

--- packages/capnp-plain/src/message.rs
+++ packages/capnp-plain/src/message.rs
@@ -4,7 +4,7 @@ pub mod word;
 
 use anyhow::{Error, Result};
 
-use crate::CapnpPlainStruct;
+use crate::{message::tree::struct_node::get_struct_stack_size, CapnpPlainStruct};
 
 use self::{
     segment::Segment,
@@ -55,13 +55,45 @@ impl Message {
         };
         Message { segments }
     }
-    pub fn read_root<T: CapnpPlainStruct>(&self) -> Result<T> {
+    pub fn to_bytes(&self, options: EncodingOptions) -> Vec<u8> {
+        let mut output = vec![];
+        let segment_count = self.segments.len();
+        if options.segment_table {
+            assert!(segment_count >= 1);
+            push_u32(&mut output, segment_count - 1);
+            for segment in &self.segments {
+                push_u32(&mut output, segment.words.len());
+            }
+            if segment_count % 2 == 0 {
+                push_u32(&mut output, 0);
+            }
+        } else {
+            assert!(segment_count == 1);
+        }
+        for segment in &self.segments {
+            for word in &segment.words {
+                output.extend_from_slice(&word.0);
+            }
+        }
+        if options.pack {
+            Word::to_packed_bytes(&output)
+        } else {
+            output
+        }
+    }
+    pub fn to_root<T: CapnpPlainStruct>(&self) -> Result<T> {
         let word_ref = WordRef::new(self, 0, 0);
         let root = Node::from_pointer(word_ref)?;
         match root {
             Node::Struct(x) => Ok(T::from_node(&x)),
             _ => Err(Error::msg("Message root is not a struct.")),
         }
+    }
+    pub fn from_root(data: &impl CapnpPlainStruct) -> Self {
+        let node = data.to_node();
+        let stack_size = get_struct_stack_size(std::slice::from_ref(&node));
+        let serializer = node.to_builder(&stack_size, 0, 0);
+        Self::from(&serializer)
     }
 }
 
@@ -73,6 +105,10 @@ fn get_u32(words: &[Word], i: usize) -> u32 {
         [word[4], word[5], word[6], word[7]]
     };
     u32::from_le_bytes(a)
+}
+
+fn push_u32(output: &mut Vec<u8>, item: usize) {
+    output.extend_from_slice(&(item as u32).to_le_bytes());
 }
 
 impl std::fmt::Debug for Message {
